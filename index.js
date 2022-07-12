b0aec11fd2ae68be3215026bac26c6c145259a3a
Shuvo Haldar <86658217+shuvo-h@users.noreply.github.com>
2022-07-12 12:54:05 +0600

    cors fix

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5000
+
+app.use(cors())
+
 
 app.get('/cook', (req, res) => {
     
