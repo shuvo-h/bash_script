3130f54350c84989e9cb0716d82b2fa34fc97d5a
Shuvo Haldar <86658217+shuvo-h@users.noreply.github.com>
2022-07-12 15:02:21 +0600

    removed same site

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const app = express()
 require("dotenv").config()
 const port = process.env.PORT || 5000
 
+var cookie = require('cookie');
+
 app.use(cors({
   origin: ["https://simple-test-dev.netlify.app","http://localhost:3000"],
   credentials: true
 }))
 
 app.get('/cook', (req, res) => {
-    
-    res.cookie("testSiteCook","df44g1gee65g4eg1",{
+    /*
+      res.cookie("testSiteCook","df44g1gee65g4eg1",{
+          httpOnly: true,
+          expires: new Date(Date.now() + 25892000000),
+          sameSite: "strict",
+          // secure: true
+      }).json({a:"Abc"})
+    */
+      res.setHeader('Set-Cookie', cookie.serialize('name', String("query.name"), {
         httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        // sameSite: "none",
-        // secure: true
-    }).json({a:"Abc"})
+        maxAge: 60 * 60 * 24 * 7 // 1 week
+      }))
+      res.json({we:"Ended Response"})
 
 })
 
