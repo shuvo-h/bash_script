11be68d8f868180e21b4371f8f56ed1d949f3508
Shuvo Haldar <86658217+shuvo-h@users.noreply.github.com>
2022-07-12 14:13:54 +0600

    secure ctatus change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/cook', (req, res) => {
         httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         sameSite: "none",
-        secure: false
+        secure: true
     }).json({a:"Abc"})
 
 })
